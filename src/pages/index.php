0d7a1c1d6377eabe74d1ee66828fb9e634145687
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-26 13:34:12 +0200

    Eliminado commit innecesario

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
     <link rel="stylesheet" href="../css/style.css">
-    <?php require_once("../services/darkMode.php") ?>
 </head>
 
 <body class="<?= $dark ?>">
