2bbecbfc46ca055df9d66d0e2e7a90d4ec9ec121
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-26 14:08:33 +0200

    Añadido modo oscuro al body

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,8 +8,14 @@
     <?php require("../services/imports.php") ?>
 </head>
 
-<body class="<?= $dark ?>">
+<body class="dark:bg-neutral-800 dark:text-white">
     <?php require("../components/header.php") ?>
+
+    <main class="pt-5">
+        <h1 class="text-4xl text-center">PokeFull</h1>
+        <h2 class="text-2xl text-center">Tu pagina de pokemons de confianza</h2>
+        <main>
+
 </body>
 
 </html>
