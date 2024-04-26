234773c0f2bb1e4412a4e21ebb7d1f52bca0f350
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-26 13:42:58 +0200

    Mejorado el modo oscuro y claro

--- src/services/darkMode.js
+++ src/services/darkMode.js
@@ -1,15 +1,16 @@
 let darkModeSelector = document.getElementById("toggle-dark-mode");
+darkModeSelector.innerHTML = "<i class='bx bxs-moon'></i>"
 let mode = "light"
 
-darkModeSelector.addEventListener("click", () => {
-    toggleDarkMode();
-});
+darkModeSelector.addEventListener("click", toggleDarkMode)
 
 function toggleDarkMode() {
     if (mode === "dark") {
+        darkModeSelector.innerHTML = "<i class='bx bxs-moon'></i>"
         document.body.classList.remove("dark");
         mode = "light";
     } else {
+        darkModeSelector.innerHTML = "<i class='bx bxs-sun' ></i>"
         document.body.classList.add("dark");
         mode = "dark";
     }
