0d58bea293b812ad737035ca01d27d61ee493d6c
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-26 10:15:46 +0200

    Se han añadido tests

--- src/test/tailwind.test.php
+++ src/test/tailwind.test.php
@@ -1,28 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-
-
-<?php
-
-require_once("../../vendor/autoload.php");
-
-$dotenv = Dotenv\Dotenv::createImmutable("../../");
-$dotenv->load();
-
-?>
-
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Tailwind test</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
-
-<body>
-    <h1 class="text-4xl text-pink-800 hover:bg-orange-600 transition-colors">A</h1>
-    <?= $_ENV["TEST"] ?>
+<body class="flex justify-center content-center items-center h-[100vh]">
+    <h1 class="text-4xl text-pink-800 hover:bg-orange-600 transition-colors text-center w-max h-max">Si ves esto centrado y con colores tailwind funciona</p>
 </body>
-
 </html>
