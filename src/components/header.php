23f71e46c911ee392c1b09645b8a38d027fa67a8
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-30 13:55:40 +0200

    Se ha mejorado el header

--- src/components/header.php
+++ src/components/header.php
@@ -1,7 +1,7 @@
 <header class="flex items-center p-4 flex-col md:flex-row md:justify-between bg-red-600 text-white dark:bg-purple-950">
         <a href="./index.php"><img src="../assets/img/logo.webp" alt="Logo de PokeFull" class="w-24"></a>
-        <nav class="flex justify-between gap-3 flex-row  p-2">
-                <div class="bg-red-800 dark:bg-purple-800">
+        <nav class="flex gap-3 flex-row p-2">
+                <div class="flex flex-row justify-between p-3 gap-3 bg-red-800 dark:bg-purple-800">
                         <a href="../pages/index.php" id="home">Home</a>
                         <p class="text-red-500 dark:text-purple-500">|</p>
                         <a href="../pages/wiki.php" id="wiki">Wiki</a>
