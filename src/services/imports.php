2b4c1b12161c202184f275a7132ac28ea90b5ce8
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-30 14:18:55 +0200

    Arreglado el loader

--- src/services/imports.php
+++ src/services/imports.php
@@ -1,2 +1,5 @@
 <link rel="stylesheet" href="../css/style.css">
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<?php
+require_once("../services/loader.php");
+?>
