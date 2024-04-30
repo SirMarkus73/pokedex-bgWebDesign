c52296a2f13fb5005e3428a1f08c65e652a1d543
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-30 13:53:47 +0200

    Se ha mejorado el header

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -3,16 +3,16 @@ session_start();
 
 
 if (isset($_SESSION["usuario"])) : ?>
-    <div class="flex justify-between gap-3 flex-row bg-red-800 dark:bg-purple-800 p-2">
-        <p><?= $_SESSION["usuario"] ?></p>
-        <p class="text-red-500 dark:text-purple-500">|</p>
-        <a href="../services/logout.php" id="logout">Cerrar sesión</a>
-    </div>
+
+    <p><?= $_SESSION["usuario"] ?></p>
+    <p class="text-red-500 dark:text-purple-500">|</p>
+    <a href="../services/logout.php" id="logout">Cerrar sesión</a>
+
 
 <?php else : ?>
-    <div class="flex justify-between gap-3 flex-row bg-red-800 dark:bg-purple-800 p-2">
-        <a href="../pages/login.php" id="login">Login</a>
-        <p class="text-red-500 dark:text-purple-500">|</p>
-        <a href="../pages/register.php" id="signup">Signup</a>
-    </div>
+
+    <a href="../pages/login.php" id="login">Login</a>
+    <p class="text-red-500 dark:text-purple-500">|</p>
+    <a href="../pages/register.php" id="signup">Signup</a>
+
 <?php endif;
