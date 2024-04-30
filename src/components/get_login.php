61ca8426fc79acefd8b57b453d3d0888fcbfd6ce
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-30 13:44:39 +0200

    se ha mejorado la barra de separación

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -3,8 +3,9 @@ session_start();
 
 
 if (isset($_SESSION["usuario"])) : ?>
-    <div class="flex justify-between gap-3 flex-row bg-red-800 border-red-700 dark:bg-purple-800 dark:border-purple-800 divide-x-2 p-1">
+    <div class="flex justify-between gap-3 flex-row bg-red-800 dark:bg-purple-800 p-2">
         <p><?= $_SESSION["usuario"] ?></p>
+        <p class="text-red-500 dark:text-purple-500">|</p>
         <a href="../services/logout.php" id="logout">Cerrar sesión</a>
     </div>
 
