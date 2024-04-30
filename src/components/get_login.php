36826b71779b20f0ecc736fa9db009e037e7500e
MarkusPortable <marcosbh2002bh@gmail.com>
2024-04-30 13:41:55 +0200

    Mejorado

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -3,7 +3,7 @@ session_start();
 
 
 if (isset($_SESSION["usuario"])) : ?>
-    <div class="flex gap-1 bg-red-800 border-red-700 dark:bg-purple-800 dark:border-purple-800 divide-x-2 p-1">
+    <div class="flex justify-between gap-3 flex-row bg-red-800 border-red-700 dark:bg-purple-800 dark:border-purple-800 divide-x-2 p-1">
         <p><?= $_SESSION["usuario"] ?></p>
         <a href="../services/logout.php" id="logout">Cerrar sesión</a>
     </div>
