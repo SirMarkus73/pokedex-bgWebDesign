beebc1905e22f5801ce8c79cc7d14114d3ec2c17
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 11:54:29 +0200

    Mejorado los botones de inicio de sesion

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,23 +1,23 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
-    <details class="dropdown">
-        <summary><?= $_SESSION["usuario"] ?></summary>
+    <div class="dropdown">
+        <button class="btn" tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
         </ul>
-    </details>
+    </div>
 
 <?php else : ?>
 
-    <details class="dropdown">
-        <summary>User</summary>
+    <div class="dropdown">
+        <button class="btn" tabindex="0">User</button>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/login.php" id="login">Login</a></li>
             <li><a href="../pages/register.php" id="signup">Signup</a></li>
         </ul>
-    </details>
+    </div>
 
 <?php endif;
