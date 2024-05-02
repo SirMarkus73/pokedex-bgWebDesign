0936918328ae61e9f39ed37646365de113a34916
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 11:55:31 +0200

    Mejorado el boton

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,7 +1,7 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
     <div class="dropdown">
-        <button class="btn" tabindex="0"><?= $_SESSION["usuario"] ?></button>
+        <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
@@ -12,7 +12,7 @@
 <?php else : ?>
 
     <div class="dropdown">
-        <button class="btn" tabindex="0">User</button>
+        <button tabindex="0">User</button>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/login.php" id="login">Login</a></li>
