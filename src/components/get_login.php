493ce5ec3d401c6885d5f3ad22f3a2c690071dcd
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 11:59:44 +0200

    mejorado la pantalla de carga

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -3,7 +3,7 @@
     <div class="dropdown">
         <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[-1]" tabindex="0">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
         </ul>
@@ -14,7 +14,7 @@
     <div class="dropdown">
         <button tabindex="0">User</button>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[-1]" tabindex="0">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/login.php" id="login">Login</a></li>
             <li><a href="../pages/register.php" id="signup">Signup</a></li>
         </ul>
