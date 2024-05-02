d70ee69b90bec0b299309f8d7dd26bce929fa7ce
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 10:39:07 +0200

    Creado dropdown

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -2,6 +2,15 @@
 
 if (isset($_SESSION["usuario"])) : ?>
 
+    <details class="dropdown">
+        <summary>User</summary>
+
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-52">
+            <li><a href="../pages/user.php" id="setting">Settings</a></li>
+            <li><a href="../services/logout.php" id="logout">Logout</a></li>
+        </ul>
+    </details>
+
     <p><?= $_SESSION["usuario"] ?></p>
     <p class="text-red-500 dark:text-purple-500">|</p>
     <a href="../services/logout.php" id="logout">Cerrar sesión</a>
@@ -9,8 +18,13 @@ if (isset($_SESSION["usuario"])) : ?>
 
 <?php else : ?>
 
-    <a href="../pages/login.php" id="login">Login</a>
-    <p class="text-red-500 dark:text-purple-500">|</p>
-    <a href="../pages/register.php" id="signup">Signup</a>
+    <details class="dropdown">
+        <summary>User</summary>
+
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-52">
+            <li><a href="../pages/login.php" id="login">Login</a></li>
+            <li><a href="../pages/register.php" id="signup">Signup</a></li>
+        </ul>
+    </details>
 
 <?php endif;
