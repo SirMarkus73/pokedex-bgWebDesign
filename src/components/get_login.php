5d89e233807bae637b7affb1ce325170debd942f
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 10:40:54 +0200

    Mejorado el tamaño del dropdown

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -5,7 +5,7 @@ if (isset($_SESSION["usuario"])) : ?>
     <details class="dropdown">
         <summary>User</summary>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-52">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
         </ul>
@@ -21,7 +21,7 @@ if (isset($_SESSION["usuario"])) : ?>
     <details class="dropdown">
         <summary>User</summary>
 
-        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-52">
+        <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit">
             <li><a href="../pages/login.php" id="login">Login</a></li>
             <li><a href="../pages/register.php" id="signup">Signup</a></li>
         </ul>
