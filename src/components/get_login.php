0310b5789df1da818a9fd5d5196cae94834cc8df
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 11:49:58 +0200

    Arreglado el inicio de sesion

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,20 +1,13 @@
-<?php
-
-if (isset($_SESSION["usuario"])) : ?>
+<?php if (isset($_SESSION["usuario"])) : ?>
 
     <details class="dropdown">
-        <summary>User</summary>
+        <summary><?= $_SESSION["usuario"] ?></summary>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
         </ul>
     </details>
-
-    <p><?= $_SESSION["usuario"] ?></p>
-    <p class="text-red-500 dark:text-purple-500">|</p>
-    <a href="../services/logout.php" id="logout">Cerrar sesión</a>
-
 
 <?php else : ?>
 
