60a696c1bece751b535c99ba257a835ec81fbf27
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 12:49:56 +0200

    Prueba de ping

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,9 +1,9 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
     <div class="dropdown">
-        <span class="relative flex w-fit">
-            <span class="animate-ping absolute inline-flex h-1/2 w-1/3 rounded-full bg-red-700 dark:bg-fuchsia-700 opacity-75"></span>
-            <button class="relative inline-flex rounded-full h-fit " tabindex="0"><?= $_SESSION["usuario"] ?></button>
+        <span class="relative flex h-3 w-3">
+            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500">Prueba</span>
         </span>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
