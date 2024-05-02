6135c34ac6bfcb56bcf00a705be35215a5c6f80e
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 12:53:13 +0200

    Ping v1

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,11 +1,13 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
-    <div class="dropdown">
-        <span class="relative flex h-3 w-3">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-fit w-fit bg-sky-500">Prueba</span>
-        </span>
+    <div class="dropdown w-fit h-fit">
 
+        <button>
+            <span class="relative flex h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-fit w-fit bg-sky-500"><?= $_SESSION["usuario"] ?></span>
+            </span>
+        </button>
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
