7e8d078bf9fc6e62c1cc43cac318219fc0715955
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 12:35:20 +0200

    Mejorado el ping

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -2,8 +2,8 @@
 
     <div class="dropdown">
         <span class="relative flex w-fit">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-700 dark:bg-fuchsia-700 opacity-75"></span>
-            <button class="relative inline-flex rounded-full h-3 w-3 " tabindex="0"><?= $_SESSION["usuario"] ?></button>
+            <span class="animate-ping absolute inline-flex h-1/6 w-1/6 rounded-full bg-red-700 dark:bg-fuchsia-700 opacity-75"></span>
+            <button class="relative inline-flex rounded-full h-fit " tabindex="0"><?= $_SESSION["usuario"] ?></button>
         </span>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
