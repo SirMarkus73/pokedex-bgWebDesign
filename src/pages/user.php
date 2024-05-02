6b1d6ada1745fcec50401adf046195b9e2b92c72
Sergi <elsergigaga@gmail.com>
2024-05-02 12:53:09 +0200

    Users pagas

---
+++ src/pages/user.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require_once("../services/imports.php") ?>
+
+</head>
+<body class="dark:bg-neutral-800 dark:text-white">
+<?php require("../components/header.php") ?>
+
+</body>
+</html> 
