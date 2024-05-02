8927dfec5b2b0dc959015db1416216ec536e1ce1
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 12:50:58 +0200

    Prueba 2

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -3,7 +3,7 @@
     <div class="dropdown">
         <span class="relative flex h-3 w-3">
             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500">Prueba</span>
+            <span class="relative inline-flex rounded-full h-fit w-fit bg-sky-500">Prueba</span>
         </span>
 
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
