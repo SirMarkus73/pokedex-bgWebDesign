6e1b6b2d3c31f040238dd0a098130de6630aaa00
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-02 12:56:54 +0200

    Se ha vuelto a como estaba antes

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,13 +1,8 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
-    <div class="dropdown w-max h-max bg-red-800 dark:bg-purple-800">
+    <div class="dropdown">
+        <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
-        <button>
-            <span class="relative flex h-3 w-3">
-                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                <span class="relative inline-flex rounded-full h-fit w-fit bg-sky-500"><?= $_SESSION["usuario"] ?></span>
-            </span>
-        </button>
         <ul class="p-2 shadow menu dropdown-content bg-slate-700 rounded-box w-fit z-[1]" tabindex="0">
             <li><a href="../pages/user.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
