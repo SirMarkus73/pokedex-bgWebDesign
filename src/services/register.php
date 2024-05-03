e4388d76abd727bf3c508db3199f3e983f41c646
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 13:37:31 +0200

    Arreglado register

--- src/services/register.php
+++ src/services/register.php
@@ -12,7 +12,7 @@ $successful = post("successful", "false");
 
 if ($successful == "true") {
     $conn = mysqli_connect($_ENV["DB"], $_ENV["USER"], $_ENV["PASSWORD"], $_ENV["DBNAME"]);
-    $sql = "INSERT INTO usuarios (usuario, contraseña, email) VALUES ('$username', '$hash_password', '$email');";
+    $sql = "INSERT INTO usuarios (usuario, contraseña, email) VALUES ($username, $hash_password, $email);";
 
     $result = mysqli_query($conn, $sql);
 
