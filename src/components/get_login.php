5d9a6a3b7053034c729cfbf1d18f168d70876a0c
Markus <marcosbh2002bh@gmail.com>
2024-05-03 18:11:35 +0200

    añadido settings cuando no estas logeado

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -1,23 +1,24 @@
 <?php if (isset($_SESSION["usuario"])) : ?>
 
-    <div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
-        <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
+<div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
+    <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
-        <ul class="p-2 shadow menu dropdown-content   rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800" tabindex="0">
-            <li><a href="../pages/user.php" id="setting">Settings</a></li>
-            <li><a href="../services/logout.php" id="logout">Logout</a></li>
-        </ul>
-    </div>
+    <ul class="p-2 shadow menu dropdown-content   rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800" tabindex="0">
+        <li><a href="../pages/user.php" id="setting">Settings</a></li>
+        <li><a href="../services/logout.php" id="logout">Logout</a></li>
+    </ul>
+</div>
 
 <?php else : ?>
 
-    <div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
-        <button tabindex="0">User</button>
+<div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
+    <button tabindex="0">User</button>
 
-        <ul class="p-2 shadow menu dropdown-content  rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800" tabindex="0">
-            <li><a href="../pages/login.php" id="login">Login</a></li>
-            <li><a href="../pages/register.php" id="signup">Signup</a></li>
-        </ul>
-    </div>
+    <ul class="p-2 shadow menu dropdown-content  rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800" tabindex="0">
+        <li><a href="../pages/login.php" id="login">Login</a></li>
+        <li><a href="../pages/register.php" id="signup">Signup</a></li>
+        <li><a href="../pages/user.php" id="setting">Settings</a></li>
+    </ul>
+</div>
 
 <?php endif;
