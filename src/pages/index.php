986f5116e7d7c567b478910edb5fb18e6eeecc18
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 12:46:58 +0200

    Mejorado el index

--- src/pages/index.php
+++ src/pages/index.php
@@ -13,16 +13,16 @@
 <body>
     <?php require(__DIR__ . "/../components/header.php") ?>
 
-    <main class="pt-5">
+    <main class="pt-5 flex flex-col m-8 gap-5">
         <h1 class="text-2xl text-center py-2"> 🔥 ¡Bienvenido a PokeHub! 🔥 </h1>
         <p>
             ¿Eres un verdadero entrenador Pokémon? Entonces estás en el lugar correcto. En PokeHub, te ofrecemos una experiencia única para que puedas sumergirte en el emocionante mundo de los Pokémon. ¿Qué puedes hacer aquí? ¡Sigue leyendo!
-        </p>
-        <p>
-        <ul class="list-disc">
-            <li>📚 Explora la Wiki Pokémon: ¿Necesitas información sobre tu Pokémon favorito? Nuestra Wiki está repleta de detalles sobre habilidades, evoluciones, curiosidades y más. ¡Conviértete en un experto y sorprende a tus amigos con tus conocimientos!</li>
-            <li>🃏 Colección de Cartas Pokémon: Si eres un fanático del Juego de Cartas Coleccionables de Pokémon (JCC), estás de enhorabuena. En PokeHub, puedes explorar todas las cartas disponibles, desde las clásicas hasta las más recientes. ¿Cuál es tu carta favorita? ¡Agrega tus tesoros a tu colección virtual!</li>
-            <li>🗂️ Organiza tu Colección: ¿Tienes una caja llena de cartas Pokémon en casa? No te preocupes, en PokeHub puedes crear álbumes virtuales y organizar tus cartas por expansión, rareza o tipo. ¡Comparte tu colección con otros entrenadores y presume de tus hallazgos!
+
+
+        <ul class="list-disc list-inside">
+            <li class="py-2">📚 Explora la Wiki Pokémon: ¿Necesitas información sobre tu Pokémon favorito? Nuestra Wiki está repleta de detalles sobre habilidades, evoluciones, curiosidades y más. ¡Conviértete en un experto y sorprende a tus amigos con tus conocimientos!</li>
+            <li class="py-2">🃏 Colección de Cartas Pokémon: Si eres un fanático del Juego de Cartas Coleccionables de Pokémon (JCC), estás de enhorabuena. En PokeHub, puedes explorar todas las cartas disponibles, desde las clásicas hasta las más recientes. ¿Cuál es tu carta favorita? ¡Agrega tus tesoros a tu colección virtual!</li>
+            <li class="py-2">🗂️ Organiza tu Colección: ¿Tienes una caja llena de cartas Pokémon en casa? No te preocupes, en PokeHub puedes crear álbumes virtuales y organizar tus cartas por expansión, rareza o tipo. ¡Comparte tu colección con otros entrenadores y presume de tus hallazgos!
                 Así que, ¿estás listo para comenzar tu aventura en PokeHub? ¡Únete a nuestra comunidad y descubre todo lo que tenemos para ofrecerte! 🌟</li>
         </ul>
         </p>
