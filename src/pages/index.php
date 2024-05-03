6a34ca6b2132127629556aac3261336e7e606275
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 10:37:18 +0200

    Index arreglado

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,3 +1,4 @@
+<?php require_once(__DIR__ . "/../services/imports.php") ?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -5,16 +6,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
-    <?php require_once("../services/imports.php") ?>
+
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<body class="dark:bg-neutral-800 dark:text-white">
-    <?php require("../components/header.php") ?>
+<body>
+    <?php require(__DIR__ . "/../components/header.php") ?>
 
     <main class="pt-5">
         <h1 class="text-4xl text-center">PokeFull</h1>
         <h2 class="text-2xl text-center">Tu pagina de pokemons de confianza</h2>
-</main>
+    </main>
 
 </body>
 
