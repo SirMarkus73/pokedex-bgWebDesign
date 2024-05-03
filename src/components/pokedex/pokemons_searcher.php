2b88d95dc5e9b6e0ed41a99cfdb5348d22ebc37a
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 11:36:51 +0200

    Pokemon type colors

--- src/components/pokedex/pokemons_searcher.php
+++ src/components/pokedex/pokemons_searcher.php
@@ -8,25 +8,86 @@ $offset = get("offset", 0);
 function show_pokemon_card($name, $image_url, $ability, $stats, $types)
 { ?>
 
-    <article class="card w-96 shadow-xl bg-red-600 dark:bg-fuchsia-600">
+    <article class="card w-96 shadow-xl bg-red-600 dark:bg-fuchsia-600 text-white">
         <figure><img src="<?= $image_url ?>" alt="pokemon <?= $name ?>" /></figure>
         <div class="card-body">
             <h2 class="card-title">
                 <?= $name ?>
                 <div class="badge badge-secondary"><?= $ability ?></div>
             </h2>
-            <div class="stats">
+            <div class="stats bg-red-500 dark:bg-fuchsia-500 text-white">
                 <?php foreach ($stats as $stat) { ?>
-                    <div class="stat h-fit">
+                    <div class="stat h-fit  bg-red-500 dark:bg-fuchsia-500 text-white">
                         <div class="stat-title"><?= $stat['stat']['name'] ?></div>
                         <div class="stat-value"><?= $stat['base_stat'] ?></div>
                     </div>
                 <?php }; ?>
 
             </div>
-            <div class="card-actions justify-end">
+            <div class="card-actions justify-end text-black">
                 <?php foreach ($types as $type) { ?>
-                    <div class="badge badge-outline"><?= $type['type']['name'] ?></div>
+                    <?php
+                    $type_name = $type['type']['name'];
+                    $type_color = "bg-red-500";
+                    switch ($type_name) {
+                        case "steel":
+                            $type_color = "bg-neutral-700";
+                            break;
+                        case "fire":
+                            $type_color = "bg-orange-500";
+                            break;
+                        case "water":
+                            $type_color = "bg-blue-500";
+                            break;
+                        case "grass":
+                            $type_color = "bg-green-500";
+                            break;
+                        case "electric":
+                            $type_color = "bg-yellow-500";
+                            break;
+                        case "ice":
+                            $type_color = "bg-cyan-500";
+                            break;
+                        case "fighting":
+                            $type_color = "bg-red-600";
+                            break;
+                        case "fairy":
+                            $type_color = "bg-pink-500";
+                            break;
+                        case "psychic":
+                            $type_color = "bg-pink-600";
+                            break;
+                        case "rock":
+                            $type_color = "bg-yellow-700";
+                            break;
+                        case "ground":
+                            $type_color = "bg-amber-500";
+                            break;
+                        case "bug":
+                            $type_color = "bg-lime-500";
+                            break;
+                        case "ghost":
+                            $type_color = "bg-violet-500";
+                            break;
+                        case "normal":
+                            $type_color = "bg-neutral-500";
+                            break;
+                        case "poison":
+                            $type_color = "bg-fuchsia-500";
+                            break;
+                        case "dragon":
+                            $type_color = "bg-indigo-500";
+                            break;
+                        case "flying":
+                            $type_color = "bg-purple-500";
+                            break;
+                        case "dark":
+                            $type_color = "bg-slate-500";
+                            break;
+                    }
+                    ?>
+
+                    <div class="badge <?= $type_color ?>"><?= $type_name ?></div>
                 <?php }; ?>
             </div>
         </div>
