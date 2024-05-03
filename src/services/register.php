4cd5b05f8155c4b31f56c98bf4d9f63c08ddf829
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 13:42:33 +0200

    Prueba

--- src/services/register.php
+++ src/services/register.php
@@ -7,18 +7,21 @@ $email = post("email", "");
 $password = post("password", "");
 $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
-
 $successful = post("successful", "false");
 
 if ($successful == "true") {
     $conn = mysqli_connect($_ENV["DB"], $_ENV["USER"], $_ENV["PASSWORD"], $_ENV["DBNAME"]);
+    if (!$conn) {
+        die("Error de conexión: " . mysqli_connect_error());
+    }
+
     $sql = "INSERT INTO usuarios (usuario, contraseña, email) VALUES ('$username', '$hash_password', '$email')";
 
-    $result = mysqli_query($conn, $sql);
+    if (mysqli_query($conn, $sql)) {
+        echo "Funciona";
+    } else {
+        echo "Error al insertar datos: " . mysqli_error($conn);
+    }
 
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-
-    /* header("Location: ../pages/login.php"); */
-
-    echo "Funciona";
+    mysqli_close($conn);
 }
