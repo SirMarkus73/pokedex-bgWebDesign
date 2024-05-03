f7381f462bd615994f6ae1be663ba52f6a6e8564
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 10:38:35 +0200

    Arreglado imports

--- src/pages/wiki.php
+++ src/pages/wiki.php
@@ -1,4 +1,4 @@
-<?php require_once("../services/imports.php") ?>
+<?php require_once(__DIR__ . "/../services/imports.php") ?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -7,10 +7,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wiki Pokefull</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<body class="dark:bg-neutral-800 dark:text-white">
-    <?php require("../components/header.php") ?>
+<body>
+    <?php require(__DIR__ . "/../components/header.php") ?>
     <h1 class="title">Wiki de pokemon</h1>
     <p class="text"> Bienvenidos a nuestra PokeWiki, donde podras encontrar la informacion de tus pokemones favoritos y saber informacion sobre que es pokemon . Esperamos con ansias que os guste </p>
     <?php
