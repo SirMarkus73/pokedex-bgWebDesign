a69b7ce9465f1085b380c21037958e4876f660d8
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-03 10:33:03 +0200

    Primera aproximancion a la wiki

--- src/components/pokedex/pokemons_searcher.php
+++ src/components/pokedex/pokemons_searcher.php
@@ -5,11 +5,31 @@ require_once(__DIR__ . "/../../services/methods.php");
 $limit = get("limit", 8);
 $offset = get("offset", 0);
 
-function show_pokemon_card(string $name, string $image_url)
+function show_pokemon_card($name, $image_url, $ability, $stats, $types)
 { ?>
-    <article>
-        <img src="<?= $image_url ?>" alt="pokemon <?= $name ?>">
-        <p> <?= $name ?></p>
+
+    <article class="card w-96 shadow-xl bg-red-600 dark:bg-fuchsia-600">
+        <figure><img src="<?= $image_url ?>" alt="pokemon <?= $name ?>" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                <?= $name ?>
+                <div class="badge badge-secondary"><?= $ability ?></div>
+            </h2>
+            <div class="stats">
+                <?php foreach ($stats as $stat) { ?>
+                    <div class="stat h-fit">
+                        <div class="stat-title"><?= $stat['stat']['name'] ?></div>
+                        <div class="stat-value"><?= $stat['base_stat'] ?></div>
+                    </div>
+                <?php }; ?>
+
+            </div>
+            <div class="card-actions justify-end">
+                <?php foreach ($types as $type) { ?>
+                    <div class="badge badge-outline"><?= $type['type']['name'] ?></div>
+                <?php }; ?>
+            </div>
+        </div>
     </article>
 <?php }; ?>
 
@@ -27,14 +47,16 @@ function show_pokemons(int $limit, int $offset)
         $pokemon = json_decode($pokemon_url, true);
 
         $pokemon_name = $pokemon['name'];
-
         $pokemon_image = $pokemon['sprites']['front_default'];
+        $pokemon_ability = $pokemon["abilities"][0]["ability"]["name"];
+        $stats = $pokemon["stats"];
+        $types = $pokemon["types"];
 
-        show_pokemon_card($pokemon_name, $pokemon_image);
+        show_pokemon_card($pokemon_name, $pokemon_image, $pokemon_ability, $stats, $types);
     }
 }
 ?>
-<section class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 justify-center w-max mx-auto gap-20 items-center">
+<section class="gap-3 mx-auto my-10 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 justify-center items-center">
     <?php show_pokemons($limit, $offset) ?>
 </section>
 
