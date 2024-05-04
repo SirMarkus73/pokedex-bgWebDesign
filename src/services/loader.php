fa542c2e7d429397f4b19b4cc63b285c122c8e43
Markus <marcosbh2002bh@gmail.com>
2024-05-04 23:58:06 +0200

    Ahora mientras la pagina carga se esconde la etiqueta main para evitar barras de desplazamieinto durante la carga

--- src/services/loader.php
+++ src/services/loader.php
@@ -1,23 +1,3 @@
-<script>
-    async function loader() {
-        window.addEventListener("beforeunload", () => {
-            const loader = document.getElementById("loader");
-            loader.classList.remove("hidden");
-            loader.classList.add("absolute");
-
-
-        })
-
-        window.addEventListener("load", () => {
-            const loader = document.getElementById("loader");
-            loader.classList.add("hidden");
-            loader.classList.remove("absolute");
-        })
-    }
-
-    loader();
-</script>
-
 <div id="loader" class="absolute w-screen h-screen top-0 left-0 z-10">
     <div class="w-full flex h-screen justify-center items-center bg-white dark:bg-neutral-800">
 
@@ -25,3 +5,24 @@
         <p class="text-red-600 dark:text-fuchsia-700 text-2xl">Loading...</p>
     </div>
 </div>
+
+<script>
+
+    window.addEventListener("beforeunload", () => {
+        const loader = document.getElementById("loader");
+        const main = document.querySelector("main")
+        loader.classList.remove("hidden");
+        loader.classList.add("absolute");
+
+        main.hidden = true;
+
+
+    })
+
+    window.addEventListener("load", () => {
+        const loader = document.getElementById("loader");
+        loader.classList.add("hidden");
+        loader.classList.remove("absolute");
+    })
+
+</script>
