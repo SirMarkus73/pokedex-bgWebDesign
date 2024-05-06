788abff9a5606cdc51055ae2625b1b4437dafb0b
Markus <marcosbh2002bh@gmail.com>
2024-05-06 15:55:27 +0200

    Si falla PHP ahora redirecciona con JS

--- src/components/pokemon/get_pokemon.php
+++ src/components/pokemon/get_pokemon.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__ . "/../../services/methods.php");
+require_once (__DIR__ . "/../../services/methods.php");
 
 $name = strtolower(get("name", ""));
 $api_url = "https://pokeapi.co/api/v2/pokemon/$name";
@@ -8,13 +8,13 @@ $result = file_get_contents($api_url);
 
 $pokemon = json_decode($result, true);
 
-if ($pokemon) :
+if ($pokemon):
     $pokemon_name = $pokemon['name'];
     $image_url = $pokemon['sprites']['front_default'];
     $abilities = $pokemon["abilities"];
     $stats = $pokemon["stats"];
     $types = $pokemon["types"];
-?>
+    ?>
     <article class="card w-3/4 shadow-xl bg-red-600 dark:bg-fuchsia-600 text-white">
         <figure><img src="<?= $image_url ?>" alt="pokemon <?= $name ?>" class="w-2/5 md:w-2/6 lg:w-2/12" /></figure>
         <div class="card-body">
@@ -22,8 +22,11 @@ if ($pokemon) :
 
             <div>
                 <?php foreach ($abilities as $ability) { ?>
-                    <div class="badge bg-white dark:bg-fuchsia-500 text-black dark:text-white "><?= $ability['ability']['name'] ?></div>
-                <?php }; ?>
+                    <div class="badge bg-white dark:bg-fuchsia-500 text-black dark:text-white ">
+                        <?= $ability['ability']['name'] ?>
+                    </div>
+                <?php }
+                ; ?>
             </div>
 
             <div class="stats bg-red-500 dark:bg-fuchsia-500 text-white">
@@ -32,7 +35,8 @@ if ($pokemon) :
                         <div class="stat-title"><?= $stat['stat']['name'] ?></div>
                         <div class="stat-value"><?= $stat['base_stat'] ?></div>
                     </div>
-                <?php }; ?>
+                <?php }
+                ; ?>
 
             </div>
 
@@ -46,14 +50,22 @@ if ($pokemon) :
                     ?>
 
                     <div class="badge text-white <?= $type_color ?>"><?= $type_name ?></div>
-                <?php }; ?>
+                <?php }
+                ; ?>
             </div>
         </div>
     </article>
 
-<?php else :
+<?php else:
 
     header("Location: ../pages/error.php?title=Pokemon no encontrado&message=El pokemon solicitado no existe&href=../pages/wiki.php");
 
+    ?>
+
+    <script>
+        window.location.href = '../pages/error.php?title=Pokemon no encontrado&message=El pokemon solicitado no existe&href=../pages/wiki.php';
+    </script>
+
+    <?php
+    exit();
 endif;
-?>
