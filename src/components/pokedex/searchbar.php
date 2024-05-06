72b0ea572abfab439cf56bc064a0b26ef992467b
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-06 10:22:27 +0200

    Mejorados los colores de la barra de busqueda

--- src/components/pokedex/searchbar.php
+++ src/components/pokedex/searchbar.php
@@ -1,7 +1,7 @@
 <section class="flex justify-center w-max mx-auto p-4">
     <form action="pokemon.php" method="get" class="m-0">
         <label for="name"> Buscar pokemon:
-            <input type="text" name="name" id="name" class="input input-bordered text-black">
+            <input type="text" name="name" id="name" class="input input-bordered text-black bg-neutral-200 dark:bg-black dark:text-neutral-100">
         </label>
         <button type="submit" class="btn btn-ghost btn-outline btn-error dark:btn-secondary">Buscar</button>
     </form>
