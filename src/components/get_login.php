3a67bf2de6259f1311b1a60811317ba0ce712a27
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-06 12:49:23 +0200

    Arreglada ruta del header

--- src/components/get_login.php
+++ src/components/get_login.php
@@ -4,7 +4,7 @@
         <button tabindex="0"><?= $_SESSION["usuario"] ?></button>
 
         <ul class="p-2 shadow menu dropdown-content   rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800" tabindex="0">
-            <li><a href="../pages/user.php" id="setting">Settings</a></li>
+            <li><a href="../pages/settings.php" id="setting">Settings</a></li>
             <li><a href="../services/logout.php" id="logout">Logout</a></li>
             <?php require_once(__DIR__ . "/dark_mode_selector.php") ?>
         </ul>
