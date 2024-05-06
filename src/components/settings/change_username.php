8cd6ce451a3e59a01b66823a2358f28fc314b3c1
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-06 12:56:28 +0200

    Se ha arreglado el change username

--- src/components/settings/change_username.php
+++ src/components/settings/change_username.php
@@ -5,9 +5,10 @@
         <div class="modal-box flex flex-col border border-gray-300 p-2 rounded-md mb-4 text-red-700 bg-neutral-50 dark:text-neutral-100 dark:bg-neutral-900 gap-5">
             <h3 class="font-bold text-lg">Cambiar nombre de usuario</h3>
             <form action="../services/change_username.php" method="post">
-                <label for="newUsername">Nuevo usuario:
-                    <input class="bg-neutral-200 dark:bg-black" type="password" name="newUsername" required>
+                <label for="new-username">Nuevo usuario:
+                    <input class="bg-neutral-200 dark:bg-black" type="password" name="new-username" required>
                 </label>
+                <input type="hidden" name="username" value="<?= $username ?>">
                 <button type="submit" class="btn btn-outline btn-error dark:btn-secondary">Cambiar usuario</button>
             </form>
             <div class="modal-action">
