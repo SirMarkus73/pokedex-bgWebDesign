1a37dc92077cef38a39360c075c443985459576f
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-07 14:56:01 +0200

    test

--- src/src_route.php
+++ src/src_route.php
@@ -1,8 +1,8 @@
 <?php
-require_once(__DIR__ . "/services/env.php");
+/* require_once(__DIR__ . "/services/env.php");
 
 if ($_ENV["DEPLOY"] == "YES") {
     define("SRC_ROUTE", $_ENV["DEPLOY_URL"]);
-} else {
-    define("SRC_ROUTE", substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT'])));
-}
+} else { */
+define("SRC_ROUTE", substr(__DIR__, strlen($_SERVER['DOCUMENT_ROOT'])));
+/* } */
