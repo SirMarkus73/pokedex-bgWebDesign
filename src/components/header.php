7be83217780b90e6eacbdc145a4b2a452c73e716
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-07 13:08:41 +0200

    El header tambien usa la funcion render component ahora

--- src/components/header.php
+++ src/components/header.php
@@ -1,3 +1,5 @@
+<?php require_once(__DIR__ . "/../services/components_renderer.php") ?>
+
 <header class="flex items-center p-4 flex-col md:flex-row md:justify-between bg-red-600 text-white dark:bg-purple-950">
     <a href="./index.php"><img src="../assets/img/logo.webp" alt="Logo de PokeFull" class="w-24"></a>
     <nav class="flex gap-3 flex-row p-2">
@@ -6,7 +8,7 @@
             <p class="text-red-500 dark:text-purple-500">|</p>
             <a href="../pages/wiki.php" id="wiki">Wiki</a>
             <p class="text-red-500 dark:text-purple-500">|</p>
-            <?php require_once(__DIR__ . "/get_login.php") ?>
+            <?php render_component("get_login") ?>
         </div>
     </nav>
 
