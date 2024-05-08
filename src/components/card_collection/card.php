4bd19d952027d259aafeb9f6ad48ef236181fb2e
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-08 13:59:43 +0200

    Arreglado sistema de busqueda

--- src/components/card_collection/card.php
+++ src/components/card_collection/card.php
@@ -3,25 +3,22 @@
 require __DIR__ . "/../../services/methods.php";
 
 // Obtén el ID de la carta desde la URL
-$cardName = get("name", null);
+$cardName = str_replace("*", " ", get("name", null));
 $cardNumber = get("number", null);
 $cardHP = get("hp", null);
 
 if (isset($cardHP)) {
-    $hpParam = "%20hp=$cardHP";
+    $hpParam = "%20hp:320";
 } else {
     $hpParam = "";
 }
 
 if (isset($cardName) && isset($cardNumber)):
     // Realiza una solicitud a la API de Pokémon
-    $apiUrl = str_replace(
-        " ",
-        "*",
-        "https://api.pokemontcg.io/v2/cards?q=name:$cardName%20number:$cardNumber$hpParam&select=name,hp,number,images"
-    );
 
-    $response = file_get_contents($apiUrl);
+    $api_url = "https://api.pokemontcg.io/v2/cards?q=name:*$cardName*%20number:$cardNumber$hpParam&pageSize=1&select=images,name";
+
+    $response = file_get_contents($api_url);
 
     $data = json_decode($response, true);
 
--- src/components/card_collection/searchbar.php
+++ src/components/card_collection/searchbar.php
@@ -16,20 +16,23 @@ $cards = $results["data"];
 </datalist>
 
 
-<section class="flex justify-center w-max mx-auto p-4">
-    <form method="get" class="m-0">
-        <label for="name"> Buscar Carta:
+<section class="border border-red-500 dark:border-fuchsia-500 m-5 mb-0">
+    <legend class="text-center text-2xl py-5">Buscar Carta</legend>
+    <form method="get" class="flex justify-center w-max mx-auto py-2 gap-4 text-center m-0">
+        
+
+        <label for="name"> Nombre:
             <input type="text" name="name" id="name" list="cardList" autocomplete="on" required
                 class="input input-bordered text-black bg-neutral-200 dark:bg-black dark:text-neutral-100">
         </label>
         
     
-        <label for="number"> Buscar por Número:
+        <label for="number"> Numero:
                 <input type="number" name="number" id="number" autocomplete="off" required
                 class="input input-bordered text-black bg-neutral-200 dark:bg-black dark:text-neutral-100">
         </label>
 
-        <label for="hp"> Buscar por Vida:
+        <label for="hp"> Vida (Opcional):
                 <input type="number" name="hp" id="hp" autocomplete="off"
                 class="input input-bordered text-black bg-neutral-200 dark:bg-black dark:text-neutral-100">
         </label>
