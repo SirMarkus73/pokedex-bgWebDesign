3ca213c5810fd4ba9cae20d9d46f3bd240791f66
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-09 12:39:37 +0200

    Eliminado formatador pretier

--- src/components/card_collection/card.php
+++ src/components/card_collection/card.php
@@ -6,20 +6,21 @@ $response = file_get_contents($json_route);
 
 $cards = json_decode($response, true);
 
-if ($id != null):
-    foreach ($cards["data"] as $card) {
-        if ($card["id"] == $id) {
-            $card_selected = $card;
-        }
-    } ?>
 
-<section class=" m-5 mb-0 w-full mx-auto">
-    <a href="<?= $card_selected["img"] ?>">
-        <img src="<?= $card_selected["img"] ?>" alt="Pokemon <?= $card_selected[
-    "name"
-] ?>" class="w-1/3 mx-auto border-4 border-red-500 dark:border-fuchsia-500 rounded-3xl ">
-    </a>
-</section>
+foreach ($cards["data"] as $card) {
+    if ($card["id"] == $id) {
+        $card_selected = $card;
+    }
+}
 
-<?php
-endif; ?>
+?>
+
+<?php if (isset($card_selected)) : ?>
+
+    <section class=" m-5 mb-0 w-full mx-auto" target="_blank">
+        <a href="<?= $card_selected["img"] ?>">
+            <img src="<?= $card_selected["img"] ?>" alt="Pokemon <?= $card_selected["name"] ?>" class="w-1/3 mx-auto border-4 border-red-500 dark:border-fuchsia-500 rounded-3xl">
+        </a>
+    </section>
+
+<?php endif; ?>
