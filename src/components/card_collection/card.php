c118f84b57f8b3c79f97c10959a7ba45cad5f7a5
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-13 12:54:30 +0200

    Test de commit desde jetbrains ide

--- src/components/card_collection/card.php
+++ src/components/card_collection/card.php
@@ -9,7 +9,7 @@ foreach ($cards as $card) {
 
 <?php if (isset($card_selected)) : ?>
 
-    <section class=" mb-0 m-10 w-3/4 mx-auto border border-red-500 dark:border-fuchsia-500 rounded-lg flex flex-col justify-center p-3 gap-3">
+    <section class=" mb-0 m-10 w-max mx-auto flex flex-col justify-center p-3 gap-3">
         <a href="<?= $card_selected["img"] ?>" target="_blank">
             <img src="<?= $card_selected["img"] ?>" alt="Pokemon <?= $card_selected["name"] ?>" class="w-1/3 mx-auto border-4 border-red-500 dark:border-fuchsia-500 rounded-3xl">
         </a>
