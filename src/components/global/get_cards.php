6d62a7290414a06d697c23b003a656bc93878111
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-15 10:30:52 +0200

    Mejorada la navegación del header

---
+++ src/components/global/get_cards.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . "/../../services/components_renderer.php";
+require_once __DIR__ . "/../../src_route.php";
+?>
+
+
+<?php if (isset($_SESSION["usuario"])) : ?>
+
+    <div class="dropdown dropdown-hover dropdown-bottom dropdown-end w-max">
+        <button tabindex="0">
+            Cards
+        </button>
+
+        <ul class="p-2 shadow menu dropdown-content rounded-box w-fit z-[1] bg-red-800 dark:bg-purple-800"
+            tabindex="0">
+            <li><?= $_SESSION["usuario"] ?></li>
+            <hr class="w-2/3 text-center">
+            <li><a href="<?= SRC_ROUTE ?>/pages/cards/index.php" id="setting">Todas las cartas</a></li>
+            <li><a href="<?= SRC_ROUTE ?>/pages/cards/collection.php" id="logout">Mi colección</a></li>
+        </ul>
+    </div>
+
+<?php else : ?>
+
+    <p>Cards</p>
+
+<?php endif;
--- src/components/global/header.php
+++ src/components/global/header.php
@@ -8,12 +8,12 @@ require_once __DIR__ . "/../../src_route.php";
     <a href="<?= SRC_ROUTE ?>/pages/index.php"><img src="<?= SRC_ROUTE ?>/assets/img/logo.webp" alt="Logo de PokeFull"
                                                     class="w-24"></a>
     <nav class="flex gap-3 flex-row p-2">
-        <div class="flex flex-row justify-between p-3 gap-3 bg-red-800 dark:bg-purple-800 items-center">
+        <div class="flex flex-row justify-between p-3 gap-3 bg-red-800 dark:bg-purple-800 items-center ">
             <a href="<?= SRC_ROUTE ?>/pages/index.php" id="home">Home</a>
             <p class="text-red-500 dark:text-purple-500">|</p>
             <a href="<?= SRC_ROUTE ?>/pages/wiki/index.php" id="wiki">Wiki</a>
             <p class="text-red-500 dark:text-purple-500">|</p>
-            <a href="<?= SRC_ROUTE ?>/pages/cards/index.php" id="cards">Cards</a>
+            <?php render_component("global/get_cards"); ?>
             <p class="text-red-500 dark:text-purple-500">|</p>
             <?php render_component("global/get_login"); ?>
         </div>
