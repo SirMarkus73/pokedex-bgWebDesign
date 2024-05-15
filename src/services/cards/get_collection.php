9c37c50cc110ed5341947d19f53206516fd48562
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-15 09:34:29 +0200

    Ahora de las cards con una funcion

--- src/services/cards/get_collection.php
+++ src/services/cards/get_collection.php
@@ -2,23 +2,32 @@
 session_start();
 require_once __DIR__ . "/../env.php";
 
-$username = $_SESSION["usuario"];
+function get_user_cards()
+{
+    $username = $_SESSION["usuario"];
+    $cards = null;
 
-if (isset($username)) {
-    $conn = mysqli_connect(
-        $_ENV["DB"],
-        $_ENV["USER"],
-        $_ENV["PASSWORD"],
-        $_ENV["DBNAME"],
-    );
+    if (isset($username)) {
+        $conn = mysqli_connect(
+            $_ENV["DB"],
+            $_ENV["USER"],
+            $_ENV["PASSWORD"],
+            $_ENV["DBNAME"],
+        );
 
-    $sql = "SELECT username, cards FROM user_cards WHERE username='$username'";
-    $result = mysqli_query($conn, $sql);
+        $sql = "SELECT username, cards FROM user_cards WHERE username='$username'";
+        $result = mysqli_query($conn, $sql);
 
-    if (mysqli_num_rows($result) == 1) {
-        $row = mysqli_fetch_assoc($result);
-        $cards = explode(" ", $row["cards"]);
-        return $cards;
+        if (mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_assoc($result);
+            $user_cards = explode(" ", $row["cards"]);
+            $cards = $user_cards;
+        }
     }
+
+    return $cards;
+
 }
-return null;
+
+
+
