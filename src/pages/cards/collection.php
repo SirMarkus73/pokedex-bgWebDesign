4d723f5c3235d1ba309a2acf6d5f97feb01f2b8d
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-16 11:41:37 +0200

    Se ha movido el boton de volver a la coleccion al inicio

--- src/pages/cards/collection.php
+++ src/pages/cards/collection.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . "/../../services/imports.php";
-
 ?>
 
 
@@ -17,12 +16,12 @@ render_component("global/loader");
 render_component("global/header");
 ?>
 <main>
-    <section class="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-        <?php render_component("card_collection/show_collection"); ?>
-    </section>
     <section class="flex justify-center text-center w-full py-3">
         <a class="btn btn-ghost btn-outline btn-error dark:btn-secondary w-2/3" href="index.php">Añadir mas
             cartas</a>
+    </section>
+    <section class="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+        <?php render_component("card_collection/show_collection"); ?>
     </section>
 </main>
 </body>
