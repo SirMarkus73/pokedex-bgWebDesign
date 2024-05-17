5042e03ffa1c38e923a6cfecf3faf5bd1ac215b4
MarkusPortable <marcosbh2002bh@gmail.com>
2024-05-17 13:24:46 +0200

    Cerrada conexion sql

--- src/services/user/delete_users.php
+++ src/services/user/delete_users.php
@@ -11,6 +11,8 @@ if ($password === $admin_password) {
 
     $db_conn = connect_to_db();
     $truncated = truncate_table_from("usuarios", $db_conn);
+    mysqli_close($db_conn);
+    
     if ($truncated) {
         header("Location:" . SRC_ROUTE . "/services/user/delete_user_cards.php?password=" . $admin_password);
         exit();
@@ -21,6 +23,7 @@ if ($password === $admin_password) {
     );
     exit();
 }
+
 
 header("Location:" . SRC_ROUTE . "/pages/index.php");
 exit();
